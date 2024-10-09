0c48ffc5c1e410e2088e1e69d7e823a3b4f37625
Angel Andonov <andonovangel1@gmail.com>
2024-10-09 21:26:39 +0200

    comment uneeded code

--- counter-saver-frontend/src/app/counter/counter.component.ts
+++ counter-saver-frontend/src/app/counter/counter.component.ts
@@ -51,16 +51,16 @@ export class CounterComponent implements OnInit {
     });
   }
 
-  refresh() {
-    console.log('refresh_token', this.authService.getRefreshToken());
-    console.log('access_token', this.authService.getAccessToken());
-    this.authService.refreshTokens().subscribe({
-      next: (res) => {
-        console.log(res);
-      },
-      error: (err) => {
-        console.log(err);
-      },
-    });
-  }
+  // refresh() {
+  //   console.log('refresh_token', this.authService.getRefreshToken());
+  //   console.log('access_token', this.authService.getAccessToken());
+  //   this.authService.refreshTokens().subscribe({
+  //     next: (res) => {
+  //       console.log(res);
+  //     },
+  //     error: (err) => {
+  //       console.log(err);
+  //     },
+  //   });
+  // }
 }
